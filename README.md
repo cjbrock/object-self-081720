85f46121337043c4414f1a3a1477805e2ee23773
cjbrock <corinna@flatironschool.com>
2020-08-25 14:44:35 -0400

    add post study group code

---
+++ README.md
@@ -0,0 +1,6 @@
+# Intro to Object Oriented Ruby
+
+## What is OO Ruby?
+
+- A way to add attributes and methods to objects
+- Allows us to make code analogous to the world around us
---
+++ examples.rb
@@ -0,0 +1,57 @@
+require 'pry'
+
+# person1 = {name: "Adam", age: 21}
+# person2 = {name: "Molly", age: 21}
+# person3 = {name: "Sascha", age: 21, hair_color: "awesome"}
+
+# puts person2[:name]
+
+# def speak(person_name)
+#     puts "Hello I'm #{person_name[:name]}, and my hair color is #{person_name[:hair_color]}"
+# end
+
+# speak(person1)
+# speak(person2)
+# speak(person3)
+
+class Person
+    def initialize(knowledge_level=20)
+        @knowledge_level=knowledge_level
+    end
+
+    # attr_reader :knowledge_level
+    # attr_writer :knowledge_level
+
+    attr_accessor :knowledge_level
+
+    # def knowledge_level #getter/reader
+    #     @knowledge_level
+    # end
+
+    # def knowledge_level=(kl) #setter/writer
+    #     @knowledge_level = kl
+    # end
+
+
+    def speak
+        puts "Hello"
+    end
+
+    def jump
+        puts "How high? Real high."
+    end
+
+    def study
+        puts self
+        @knowledge_level += 10
+        puts "Yay learning!!! Knowledge level increased to: #{knowledge_level}"
+        @knowledge_level -= 5
+        puts "You forgot some stuff!!! Knowledge level decreased to: #{knowledge_level}"
+
+    end
+
+end
+
+person4 = Person.new
+
+binding.pry
